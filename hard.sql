b1a8953a5bf11b1eaa70cd008452ed48f8f62e92
Manish mamgain <118662411+Manish72366@users.noreply.github.com>
2024-08-09 19:27:36 +0530

    Update hard.sql

--- hard.sql
+++ hard.sql
@@ -1,13 +1,18 @@
 /* Question Set 3 - Advance */
 
-/* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
+/* Q1: Find how much amount spent by each customer on artists. Write a query to return customer name, artist name and total spent */
 
-/* Steps to Solve: First, find which artist has earned the most according to the InvoiceLines. Now use this artist to find 
-which customer spent the most on this artist. For this query, you will need to use the Invoice, InvoiceLine, Track, Customer, 
-Album, and Artist tables. Note, this one is tricky because the Total spent in the Invoice table might not be on a single product, 
-so you need to use the InvoiceLine table to find out how many of each product was purchased, and then multiply this by the price
-for each artist. */
+select customer.first_name , customer.last_name,  artist.name as artist, sum(invoice_line.unit_price*invoice_line.quantity) as spent from customer
+join invoice on customer.customer_id = invoice.customer_id
+join invoice_line on invoice.invoice_id = invoice_line.invoice_id
+join track on track.track_id = invoice_line.track_id
+join album on album.album_id = track.album_id
+join artist on artist.artist_id = album.artist_id
+group by customer.first_name , customer.last_name , artist.name
+order by spent desc;
 
+-- / Q2  Find the artist who has earned the maximum money and show the distribution of the earning artist made. 
+-- Write a query to show the customer's name, artist name, and total money spent on the artist by each customer ?
 WITH best_selling_artist AS (
 	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
 	FROM invoice_line
